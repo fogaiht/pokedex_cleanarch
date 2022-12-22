d9997ded8e0612ae107e3d57cfc16a9f2d82e04d
Thiago Fernandes <thiagofpf@gmail.com>
2022-12-22 13:52:15 -0300

    Created HexColor extension

--- lib/src/shared/data/extensions.dart
+++ lib/src/shared/data/extensions.dart
@@ -58,6 +58,23 @@ extension MaterialColorGen on Color {
   }
 }
 
+extension HexColor on Color {
+  /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
+  static Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+
+  /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
+  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
+      '${alpha.toRadixString(16).padLeft(2, '0')}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
+}
+
 extension StringsExtensions on String {
   double? get toDouble => double.tryParse(this);
 
@@ -104,7 +121,7 @@ extension StringsExtensions on String {
     return value.replaceAll('.', '').replaceAll('-', '');
   }
 
-  String get capitalizeFirstofEach {
+  String get capitalizeFirst {
     final value = this;
 
     return value
